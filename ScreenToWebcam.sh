6193f1a89bd0c124d92264925e1c25886212a283
Prithvi Vishak <prithvivishak@gmail.com>
2020-12-21 15:58:49 +0530

    Used :1 environment variable instead of hardcoded value for display number

--- ScreenToWebcam.sh
+++ ScreenToWebcam.sh
@@ -6,15 +6,14 @@ InputSize: Required field
            Size of the screen to be grabbed
 OutputRes: Optional field
            Resolution the input will be scaled to in the output
-           Defaults to 1280x720"
+           Defaults to 720 columns, preserving the aspect ratio of InputSize"
 	exit 1
 fi
 
 outputRes=$( echo "$2" | sed -e s/x/:/ )
 if [ -z $outputRes ] ; then
-	outputRes="1280:720"
+	outputRes="-1:720"
 fi
 
-echo "Using output resolution $outputRes"
 sudo modprobe v4l2loopback devices=1 exclusive_caps=1 card_label=VirtCam video_nr=10
-ffmpeg -f x11grab -video_size "$1" -i :1.0 -vf vflip -vf "scale=$outputRes,hflip,format=yuv420p" -c:a copy -f v4l2 /dev/video10
+ffmpeg -f x11grab -video_size "$1" -i $DISPLAY -vf vflip -vf "scale=$outputRes,hflip,format=yuv420p" -c:a copy -f v4l2 /dev/video10
