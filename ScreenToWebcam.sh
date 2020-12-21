695bc3e28841f8ebda4e1f327469d086023fdaf8
Prithvi Vishak <prithvivishak@gmail.com>
2020-12-21 14:37:42 +0530

    Made ScreenToWebcam.sh take command line arguments for input size and output resolution

--- ScreenToWebcam.sh
+++ ScreenToWebcam.sh
@@ -1,2 +1,20 @@
+#!/bin/bash
+
+if [ -z $1 ] ; then
+	echo "Usage: ./ScreenToWebcam  <InputSize> [OutputRes]
+InputSize: Required field
+           Size of the screen to be grabbed
+OutputRes: Optional field
+           Resolution the input will be scaled to in the output
+           Defaults to 1280x720"
+	exit 1
+fi
+
+outputRes=$( echo "$2" | sed -e s/x/:/ )
+if [ -z $outputRes ] ; then
+	outputRes="1280:720"
+fi
+
+echo "Using output resolution $outputRes"
 sudo modprobe v4l2loopback devices=1 exclusive_caps=1 card_label=VirtCam video_nr=10
-ffmpeg -f x11grab -video_size 1280x720 -i :1.0 -vf vflip -vf "hflip,format=yuv420p" -c:a copy -f v4l2 /dev/video10
+ffmpeg -f x11grab -video_size "$1" -i :1.0 -vf vflip -vf "scale=$outputRes,hflip,format=yuv420p" -c:a copy -f v4l2 /dev/video10
